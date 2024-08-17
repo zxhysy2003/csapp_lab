c9971368bb5a83fb4e633b6c0938c734c4a93db0
sy <1499172392@qq.com>
2024-08-17 14:25:55 +0800

    builtin_cmd() function finish

--- shlab-handout/tsh.c
+++ shlab-handout/tsh.c
@@ -319,6 +319,20 @@ int parseline(const char *cmdline, char **argv)
  */
 int builtin_cmd(char **argv) 
 {
+    if(!strcmp(argv[0],"quit")) {
+        exit(0);
+    }
+    if(!strcmp(argv[0],"bg") || !strcmp(argv[0],"fg")) {
+        do_bgfg(argv);
+        return 1;
+    }
+    if(!strcmp(argv[0],"jobs")) {
+        listjobs(jobs);
+        return 1;
+    }
+    if(!strcmp(argv[0],"&")) {
+        return 1;
+    }
     return 0;     /* not a builtin command */
 }
 
